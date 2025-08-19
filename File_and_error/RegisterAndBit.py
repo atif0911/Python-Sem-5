7c27d908f6940aea8a15d4c965a656e88a2ba97c
atif0911 <sardaratifali2003@gmail.com>
2025-08-19 20:47:16 +0530

    Register and bit problem

--- File_and_error/RegisterAndBit.py
+++ File_and_error/RegisterAndBit.py
@@ -8,11 +8,11 @@ def find_n(x):
     return n
 while True:
     try:
-        x = int(input("Enter a number between 1 and 255: "))
+        x = int(input("Enter Number of Registers in CPU:"))
         if not check_range(x):
             raise ValueError("Number out of range.")
         n = find_n(x)
-        print(f"The value of n for {x} is {n}.")
+        print(f"The number of bits for the register field in the instruction format is {n}.")
         break
     except ValueError as e:
         print(f"Error: {e}. Please try again.")
